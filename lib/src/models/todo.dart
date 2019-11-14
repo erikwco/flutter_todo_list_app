7a0bab748cffa3c72f7300aec6241760aa4a5a0d
Erik Chacon <erikchacon@me.com>
2019-11-13 23:25:21 -0600

    Step 4 finalizado

--- lib/src/models/todo.dart
+++ lib/src/models/todo.dart
@@ -14,4 +14,14 @@ class Todo {
     this.isComplete=false
   });
 
+  Map<String, dynamic> toMap() => {
+    'name': this.name,
+    'description': this.description,
+    'isComplete': (this.isComplete) ? 1 : 0
+  };
+
+  factory Todo.fromMap(Map<String ,dynamic> data) {
+    return Todo(name: data['name'], description: data['description'], isComplete: (data['isComplete'] == '1' ? true : false));
+  }
+
 }
